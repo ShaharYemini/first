e2a5d28ed15cb3a2a6cb74b4ccb7d8544fcc1169
ShaharYemini <76625369+ShaharYemini@users.noreply.github.com>
2021-04-15 01:59:27 +0300

    update 001

---
+++ code.js
@@ -0,0 +1,13 @@
+
+function yes() {
+	document.getElementById('people').innerHTML = Object.keys(data)
+	console.log("yes")
+	}
+
+function no() {
+	console.log("no")
+	}
+
+function dontKnow() {
+	console.log("don't know")
+	}
